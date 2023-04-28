00915051bbc6ab0d70ce3a2aff77fa02393a969a
AdityaPratap24 <72563569+AdityaPratap24@users.noreply.github.com>
2023-04-28 10:54:50 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <>
+      <h1>Expense Item</h1>
+      <h5>Food Rs 10</h5>
+      <h5>Petrol Rs 100</h5>
+      <h5>Movies Rs 200</h5>
+    </>
+  );
+}
+
+export default ExpenseItem;
