2b97c700e76ebedcf65d276e29cbde68c5b5384e
AdityaPratap24 <72563569+AdityaPratap24@users.noreply.github.com>
2023-04-28 19:36:19 +0530

    added button

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,14 +1,28 @@
-import './ExpenseItem.css';
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  
+  const changeTitle = () => {
+    console.log('Clicked')
+  }
+
+  const deleteTitle = () => {
+    console.log('Delete')
+  }
+
   return (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails title = {props.title} location = {props.location} amount = {props.amount}/>
-    </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={changeTitle}>Change Title</button>
+      <button onClick={deleteTitle}>Delete</button>
+    </Card>
   );
 }
 
