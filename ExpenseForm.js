9d3f907f56c27c6521e585aa9d7aaad1faab3c5f
AdityaPratap24 <72563569+AdityaPratap24@users.noreply.github.com>
2023-04-30 12:57:50 +0530

    Update ExpenseForm.js

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enterTitle,enterTitleChange] = useState('');
     const [enterAmount,enterAmountChange] = useState('');
     const [enterDate,enterDateChange] = useState('');
@@ -21,12 +21,16 @@ const ExpenseForm = () => {
 
     const submitHandler = (e) => {
       e.preventDefault();
-      const obj = {
+      const expenseData = {
         title : enterTitle,
         amount : enterAmount,
         date : new Date(enterDate)
       }
-      console.log(obj);
+
+      props.onSaveExpenseData(expenseData);
+      enterTitleChange('');
+      enterAmountChange('');
+      enterDateChange('');
     }
 
   return (
@@ -34,18 +38,19 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}/>
+          <input type='text' value={enterTitle} onChange={titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
+          <input type='number' min='0.01' step='0.01' value={enterAmount} onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+          <input type='date' min='2019-01-01' max='2022-12-31' value={enterDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
+      <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
