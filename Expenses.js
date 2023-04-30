13bfa5943cc3d4daf04d1059aa1eaadbdc8f343c
AdityaPratap24 <72563569+AdityaPratap24@users.noreply.github.com>
2023-04-30 12:18:04 +0530

    Added conditional rendering

--- Expenses.js
+++ Expenses.js
@@ -3,6 +3,7 @@ import React,{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
+import Shimmer from "../UI/Shimmer";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -11,24 +12,21 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
+  
+  const filteredExpenses = props.items.filter((ele) => {
+    return ele.date.getFullYear().toString() === filteredYear;
+  })
   return (
     <Card className="expenses">
       <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((ele) => {
+      {filteredExpenses.length != 0 ? filteredExpenses.map((ele) => {
         return (
-          ele.date.getFullYear().toString() === filteredYear ?<ExpenseItem key={ele.id} title={ele.title} amount={ele.amount} date={ele.date}/> : " "
+          <ExpenseItem key={ele.id} title={ele.title} amount={ele.amount} date={ele.date}/>
         );
-      })}
-      
-       {/* {
-        props.items.filter((ele) => {
-          return ele.date === filteredYear ? <ExpenseItem key={ele.id} title={ele.title} amount={ele.amount} date={ele.date}/> : " "
-        })
-       } */}
+      }) : <Shimmer /> }
     </Card>
   );
 };
